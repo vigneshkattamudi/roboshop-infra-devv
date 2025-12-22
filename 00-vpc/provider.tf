3906790ce8f80c67a723b5bbbe01070194d60cbe
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-22 21:15:34 +0000

    terraform

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source ="hashicorp/aws"
+        version = "6.25.0"
+  }
+}
+
+backend "s3" {
+    bucket = "dsops84-roboshop-dev"
+    key = "roboshop-dev-vpc"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+ }
+}
+
+provider "aws" {
+    #Configuration options
+    region = "us-east-1"
+}
