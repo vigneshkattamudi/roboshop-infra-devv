8868804bb5f52669f33372f1088152987d4b3912
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-22 21:15:55 +0000

    terraform

---
+++ 20-bastion/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "bastion" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id = local.public_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-bastion"
+    }
+  )
+}
